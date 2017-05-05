8f322be4e4277e3f7323c2c3231a6f3e60bde64e
lepaulse <lepaulse@stud.ntnu.no>
2017-05-05 17:15:01 +0200

    unverified functionality, bad naming

--- fifo_tb.v
+++ fifo_tb.v
@@ -1,8 +1,7 @@
-
 `timescale 1ns/1ps
 
 module fifo_tb;
-parameter BUFFER_SIZE = 128;
+parameter BUFFER_SIZE = 16;//128;
 parameter DATA_WIDTH = 32;
 
     // Data in interface
@@ -19,9 +18,6 @@ parameter DATA_WIDTH = 32;
     wire data_out_valid;
     reg data_out_ack;
 
-    reg [32-1:0] CheckData[64-1:0]; 
-    reg [6-1:0] index;
- 
 fifo dut(.rst_in_n(rst_in_n),
          .clock_in(clock_in),
          .data_in(data_in),
@@ -43,28 +39,39 @@ initial begin // data in
     clock_in = 1'b1;
     rst_in_n = 1'b1;
     data_in_valid = 1'b1;   
-    repeat (16) begin
+    repeat (100) begin
         #5;
-        clock_in = ~clock_in;
-        if(!clock_in) // change data on negedge
-            data_in = (data_in) * 2;
+        if(!data_in_full) begin
+            clock_in = ~clock_in;
+            if(!clock_in) // change data on negedge
+                if(data_in != 0)
+                    data_in = (data_in) + 1;
+                else 
+                    data_in = 1'b1;
+        end
+        else begin
+            clock_in = ~clock_in;
+        end
     end
 end
 
-// initial begin // data out
-//     clock_out = 1'b0;
-//     rst_out_n = 1'b0;
-//     data_in = 32'h00000001;
-//     #15;
-//     clock_out = 1'b1;
-//     rst_out_n = 1'b1;
-//     repeat (16) begin
-//         #5;
-//         clock_out = ~clock_out;
-//         //data_out_valid = 1'b1;
-//         data_out_ack = 1'b1;
-//     end
-// end
+initial begin // data out
+    clock_out = 1'b0;
+    rst_out_n = 1'b0;
+    data_out_ack = 1'b0;
+    #5;
+    rst_out_n = 1'b1;
+    data_out_ack = 1'b1;
+    #220;
+    clock_out = 1'b1;
+
+
+    repeat (360) begin
+        #5;
+        clock_out = ~clock_out;
+        //data_out_valid = 1'b1;
+    end
+end
 
 
 endmodule
